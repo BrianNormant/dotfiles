e85c2277ba86146e02ec8349e731bc9d5d1f5dce
BrianArchLaptop <briannormant@gmail.com>
2023-02-23 01:44:27 -0500

    How babe

--- awesome/keys.lua
+++ awesome/keys.lua
@@ -350,7 +350,7 @@ keys.globalkeys = gears.table.join(
    -- Screenshot on Mod+Shift+S whith selection
       awful.key({modkey, "Shift"}, "s",
          function()
-            awful.util.spawn("scrot -s -f -F '%Y-%m-%d_scrot.png' -e 'mv $f ~/Pictures/' ", false)
+            awful.util.spawn("scrot -s -f -F '%Y-%m-%d_%H:%M_scrot.png' -e 'mv $f ~/Pictures/' ", false)
             naughty.notify({
                presets = naughty.config.presets.normal,
                title = "New screenshot!",
